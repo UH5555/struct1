8effbcb45a5b13d86b07575496bedae4ca0bc3cc
UH5555 <zhuhao162@163.com>
2022-03-14 18:38:25 +0800

    0314\结构体

---
+++ test.c
@@ -0,0 +1,111 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+//描述一个学生
+//名字，年龄，电话，性别
+
+//srtuct - 结构体关键字 
+//Stu - 结构体标签
+//struct Stu - 结构体类型
+
+//struct Stu
+//{
+//	//成员变量
+//	char name[20];
+//	short age;
+//	char tele[12];
+//	char sex[5];
+//}s1,s2,s3;//s1,s2,s3是三个全局变量
+
+//typedef struct Stu
+//{
+//	//成员变量
+//	char name[20];
+//	short age;
+//	char tele[12];
+//	char sex[5];
+//}Stu;
+//
+//int main()
+//{
+//	Stu s1={"张三",20,"15212345678","男"};//局部变量
+//	Stu s2={"李四",22,"15112345678","保密"};
+//	return 0;
+//}
+
+//struct S
+//{
+//	int a;
+//	char c;
+//	char arr[20];
+//	double d;
+//};
+//
+//struct T
+//{
+//	char ch[10];
+//	struct S s;
+//	char* pc;
+//};
+//
+//int main()
+//{
+//	char arr[] = "hello bit\n";
+//	struct T t = { "hehe",{100,'w',"hello world",3.14},arr};
+//	printf("%s\n", t.ch);
+//	printf("%s\n", t.s.arr);
+//	printf("%lf\n", t.s.d);
+//	printf("%s\n", t.pc);
+//	return 0;
+//}
+
+//typedef struct Stu
+//{
+//	//成员变量
+//	char name[20];
+//	short age;
+//	char tele[12];
+//	char sex[5];
+//}Stu;
+//void print1(Stu tmp)
+//{
+//	printf("name:%s\n", tmp.name);
+//	printf("age:%d\n", tmp.age);
+//	printf("tele:%s\n", tmp.tele);
+//	printf("sex:%s\n", tmp.sex);
+//}
+//
+//void print2(Stu* ps)
+//{
+//	printf("name:%s\n", ps->name);
+//	printf("age:%d\n", ps->age);
+//	printf("tele:%s\n", ps->tele);
+//	printf("sex:%s\n", ps->sex);
+//}
+//
+//int main()
+//{
+//	Stu s1={"张三",20,"15212345678","男"};//局部变量
+//	//打印结构体数据
+//	//print1 和 print2 哪个更好？
+//	print1(s1);
+//	print2(&s1);//结构体传参，要传结构体的地址
+//	return 0;
+//}
+
+//int Add(int x, int y)
+//{
+//	int z = 0;
+//	z = x + y;
+//	return z;
+//}
+//
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int ret = 0;
+//	ret = Add(a, b);
+//	return 0;
+//}
